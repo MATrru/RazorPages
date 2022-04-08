2ada8cd99dacf41de79af427e141ef999704beb4
USER <USER@DESKTOP-P7OJSL0>
2022-04-08 16:03:01 +0200

    Добавьте файлы проекта.

---
+++ GetImage/GetImage.cs
@@ -0,0 +1,73 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Drawing;
+using System.Drawing.Imaging;
+using Azure.Storage.Blobs;
+
+namespace GetImage
+{
+    public static class GetImage
+    {
+        [FunctionName("GetImage")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            BlobContainerClient blobContainerClient = new BlobContainerClient(Environment.GetEnvironmentVariable("AzureWebJobsStorage"), "sample-container");
+            blobContainerClient.CreateIfNotExists();
+
+            MemoryStream ms = new MemoryStream();
+            req.Body.CopyToAsync(ms);            
+            string text = req.Query["text"];
+
+            {
+                using (var img = Image.FromStream(ms))
+                {
+                    using (var graphic = Graphics.FromImage(img))
+                    {
+
+                        var font = new Font(FontFamily.GenericSansSerif, 38, FontStyle.Bold, GraphicsUnit.Pixel);
+                        var color = Color.FromArgb(128, 255, 255, 255);
+                        var brush = new SolidBrush(color);
+                        var point = new Point((int)(img.Width * 0.3), img.Height / 2);
+                        graphic.DrawString(text, font, brush, point);
+                        img.Save(ms, ImageFormat.Jpeg);
+
+                        ms.Position = 0;
+                        string fileName = Guid.NewGuid().ToString() + "_" + "image.jpg";
+                        await blobContainerClient.UploadBlobAsync(fileName, new BinaryData(msToByte(ms)));
+
+                        return new FileContentResult(ImageToByteArray(img), "image/jpeg");
+                    }
+                }
+            }
+
+            return new BadRequestResult();
+        }
+
+        public static byte[] msToByte(Stream input)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                input.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+
+        private static byte[] ImageToByteArray(Image image)
+        {
+            ImageConverter converter = new ImageConverter();
+            return (byte[])converter.ConvertTo(image, typeof(byte[]));
+        }
+    }
+}
+
+
---
+++ RazorPages/Pages/UploadImage.cshtml.cs
@@ -0,0 +1,68 @@
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+
+namespace RazorPages.Pages
+{
+    public class UploadImageModel : PageModel
+    {
+        private readonly ILogger<UploadImageModel> _logger;
+        private readonly IConfiguration configuration;
+
+        public UploadImageModel(ILogger<UploadImageModel> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            this.configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+            ViewData["imgBase64"] = "";
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+
+            IFormFile imageFile = Request.Form.Files["Image"];
+            string text = Request.Form["text"];
+            var url = configuration["AddImageUrl"] + "?text=" + text;
+            
+            if (imageFile != null)
+            {
+                var byteImg = msToByte(imageFile.OpenReadStream());
+
+                using (var myClient = new HttpClient())
+                using (var myRequest = new HttpRequestMessage(HttpMethod.Post, url))
+                using (var httpContent = new StreamContent(imageFile.OpenReadStream()))
+                {
+                    myRequest.Content = httpContent;
+
+                    using (var newResponse = await myClient.SendAsync(myRequest, HttpCompletionOption.ResponseHeadersRead).ConfigureAwait(false))
+                    {
+                        Stream stream = newResponse.Content.ReadAsStream();
+                        var fileBytes = msToByte(stream);
+                        var base64 = Convert.ToBase64String(fileBytes);
+                        ViewData["imgBase64"] = String.Format("data:image/jpeg;base64,{0}", base64);
+                        return Page();
+                    }
+                }
+            }
+            return Page();
+        }
+
+        public static byte[] msToByte(Stream input)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                input.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+    }
+}
